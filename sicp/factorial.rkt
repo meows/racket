76a5b5f31d52323d2ee1b8e77429bd7151968c9e
meow <meow@iceland>
2017-11-25 11:51:17 -0800

    update

--- sicp/factorial.rkt
+++ sicp/factorial.rkt
@@ -4,13 +4,9 @@
 ;
 ; linear time recursion
 
-(define (factorial n [count 1] [product 1])
-   (if (> count n)
-       product
-       (factorial n (add1 count) (* count product))))
+(define -- sub1)
 
-; Functions can viewed simultaneously from the lens of a mathematical function,
-; or from the procedural view, in which case the function is either a routine or
-; subroutine. The utility of the latter view is for the primitive counting of
-; cost, by means of evaluating the number of times a procedure, routine, or
-; co-routine may run.
+(define (factorial n [product 1])
+    (if (zero? n)
+        product
+        (factorial (-- n) (* n product))))
