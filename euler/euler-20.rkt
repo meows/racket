45b2d604995710916738cad484b06feb4a905ed5
meow <meow@iceland>
2017-11-27 08:08:20 -0800

    update

--- euler/euler-20.rkt
+++ euler/euler-20.rkt
@@ -2,10 +2,13 @@
 
 (require threading)
 (require math/number-theory)
+(require (rename-in racket/base [define fn]))
 
 ; Project Euler 20
 ;
 ; Find the sum of the digits in the number 100!
+
+(define -- sub1)
 
 (fn (factorial n [product 1])
     (if (zero? n)
@@ -13,4 +16,15 @@
         (factorial (-- n) (* n product))
 ))
 
-(define 100! (factorial 100))
+(fn (Z+->list n #:result [r (list)] #:base [b 10])
+    (if (zero? n) 
+        (reverse r)
+        (Z+->list (quotient n b) 
+                  #:result (append r (list (modulo n b))))))
+
+(fn euler-20 
+    (~> (factorial 100)
+        Z+->list
+        (apply + _)
+    )
+)
--- euler/tool.rkt
+++ euler/tool.rkt
@@ -1,11 +1,8 @@
 #lang racket
 
-(define (integer->list n #:result [r (list)] #:base [b 10])
-   (if (zero? n) r
-       (integer->list (quotient n b) 
-                      #:result (append* r (modulo n b))
-       )
-))
+(define (Z+->list n #:result [r (list)] #:base [b 10])
+    (if (zero? n) 
+        (reverse r)
+        (Z+->list (quotient n b) 
+                  #:result (append r (list (modulo n b))))))
 
-(define a (integer->list 0))
-(reverse (flatten a))
