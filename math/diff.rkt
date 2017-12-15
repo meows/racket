747825251a9febd81bbf209f7fd1377972923ae7
meow <meow@iceland>
2017-12-15 12:13:23 -0800

    update

--- math/diff.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-
-;; n sized integer list => n - 1 sized delta list
-
-(define (delta integers)
-   ()
-)
--- sicp/list-operations.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket
-
-; if item is in list x:
-; then return sublist with item at head
-(define (memq item x)
-   (cond ((null? x) false)
-         ((eq? item (car x)) x)
-         (else (memq item (cdr x)))))
