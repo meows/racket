d79964653a8c82c795cf512d22d4a38e8327a09d
meows <meow@iceland>
2018-05-02 04:18:32 -0700

    update

---
+++ euler/euler-18.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(require (rename-in racket/base [define fn]))
+(require (rename-in racket/base [define def]))
+(require math/number-theory)
+(require threading)
+(require (only-in srfi/26 cut))
+(require (only-in srfi/1 unfold-right))
+
+;; ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+;; Project Euler
+;; Problem 18
+
+; Given a bifuricating tree of random digits from 00..99, find the maximum sum
+; pathway from one end to end.
+;
+; 3
+; 7 4
+; 2 4 6  
+; 8 5 9 3
+;
+; Example max sum: 23.
+
+(fn split-by-space (cut string-split <> " "))
+
+(def file "data/euler-18.txt")
+(def input (map split-by-space (file->lines file)))
+(def euler-18
+     (map (λ (strings) (map string->number strings)) 
+          input
+     )
+)
+euler-18
