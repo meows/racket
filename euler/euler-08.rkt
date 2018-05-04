bfdfa6af862ed2d45d9362b4a63ccb02b2fd3421
meows <meow@iceland>
2018-05-04 14:48:09 -0700

    added euler 8

---
+++ euler/euler-08.rkt
@@ -0,0 +1,29 @@
+#lang racket
+
+(require (rename-in racket/base [define fn]))
+(require (rename-in racket/base [define def]))
+(require math/number-theory)
+(require (only-in srfi/26 cut))
+(require (only-in srfi/1 unfold-right unfold))
+
+;; ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+;; Project Euler
+;; Problem 8
+
+(def raw-string (apply string-append (file->lines "data/euler-08.txt")))
+
+;; ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+;; Solve
+
+(fn (N->list num [base 10])
+    (unfold-right zero?
+                  (cut remainder <> base)
+                  (cut quotient <> base)
+                  num ))
+
+(def nats (N->list (string->number raw-string)))
+
+(for/fold ([best 0] [array] #:result best)
+          ([n nats])
+          ()
+)
