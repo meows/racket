83bfdae9f2f4b98917be8a8c983e6782b559577a
meows <meow@iceland>
2018-07-24 16:55:06 -0700

    update

--- euler/euler-45.js
+++ euler/euler-45.js
@@ -1,3 +1,11 @@
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// Project Euler
+// Problem 45
+
+// Given that (triangle 285) === (pentagon 165) === (hexagon 143) === 40755.
+//
+// Find the next triangle number after 40755 that's both pentagonal and 
+// hexagonal.
 
 const pentagon = (n) => 1.5 * n*n + 1.5 * n
 const hexagon = (n) => 2*n*n + -n
