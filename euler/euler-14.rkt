b9cc1ff5baf917601dad45c12687f2455e73ecea
meows <meow@iceland>
2018-08-09 16:45:56 -0700

    update

--- euler/euler-14.rkt
+++ euler/euler-14.rkt
@@ -13,22 +13,21 @@
 (fn ++ add1)
 (fn (one? x) (= x 1))
 
-(fn (collatz [n 0] #:index [i 0])
+;; Finds the number of steps it takes for a Collatz number chain to reach 1.
+(fn (collatz [n 0] #:steps [i 0])
     (cond [(one? n) i]
-          [(even? n) (collatz (/ n 2) #:index (++ i))]
-          [else (collatz (+ 1 (* 3 n)) #:index (++ i))])
-)
+          [(even? n) (collatz (/ n 2) #:steps (++ i))]
+          [else (collatz (+ 1 (* 3 n)) #:steps (++ i))]))
 
 ;; ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 ;; Solve
 
 (fn (euler-14 #:max [max 1000000])
     (for/fold ([best 0] [seen-at 0] #:result seen-at)
-              ([n (range (++ max))] #:break (< max n))
+              ([n (in-range 1 (++ max))] #:break (< max n))
               (let ([steps (collatz n)])
                    (if (< best steps)
                        (values steps n)
-                       (values best seen-at))))
-)
+                       (values best seen-at)))))
 
 (time (euler-14))
