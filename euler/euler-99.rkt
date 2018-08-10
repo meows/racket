71fa320b49b1265bdf2edae12c1b88dc32c90d4c
meows <meow@iceland>
2018-08-09 19:34:14 -0700

    update

--- euler/euler-99.rkt
+++ euler/euler-99.rkt
@@ -3,7 +3,6 @@
 (require (rename-in racket/base [define fn]))
 (require (rename-in racket/base [define def]))
 (require math/number-theory)
-(require (only-in srfi/26 cut))
 (require (only-in srfi/1 unfold-right unfold))
 (require threading)
 
@@ -13,28 +12,28 @@
 
 (fn ++ add1)
 
-;; Raw data from file split as lines. 
+;; Raw data from file split as lines.
 (define raw-strings (file->lines "data/euler-99.txt"))
 
 ;; A list of 2-list of '(base exponent).
 (define tuples
-    (~> raw-strings 
+    (~> raw-strings
         (map (λ (string)  (string-split string  ","))   _ )
-        (map (λ (strings) (map string->number strings)) _ ))
-)
+        (map (λ (strings) (map string->number strings)) _ )))
 
 ;; Translates every (base, exponent) pair into a unique number that preserves
 ;; the orderings of power.
-(fn (log-identity base exponent) (* exponent (log base)))
+(fn (log-identity base exponent)
+    (* exponent (log base)))
 
 ;; ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 ;; Solve
 
-;; Finds the greatest value for (log-identity base exponent) from the sequence 
+;; Finds the greatest value for (log-identity base exponent) from the sequence
 ;; of <tuples> defined above.
 (for/fold ([best 0] [seen-at 0] [index 0] #:result (list best seen-at))
           ([t tuples])
           (let ([log-id (log-identity (first t) (second t))])
                (if (< best log-id)
                    (values log-id (++ index) (++ index))
-                   (values best seen-at (++ index)))))
+                   (values best   seen-at    (++ index)))))
