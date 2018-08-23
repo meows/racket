cd0ca27eaf57a7918b275f2ce76ff6672f346324
meows <meow@iceland>
2018-08-23 06:20:33 -0700

    update

--- euler/euler-20.rkt
+++ euler/euler-20.rkt
@@ -2,6 +2,7 @@
 
 (require (rename-in racket/base [define fn]))
 (require (rename-in racket/base [define def]))
+(require threading)
 
 ;; ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 ;; Project Euler
@@ -10,6 +11,7 @@
 ; Find the sum of the digits in the number 100!
 
 (fn -- sub1)
+(fn numbers->sum (curry apply +))
 
 ; finds the nth factorial
 (fn (factorial n #:product [p 1])
@@ -27,7 +29,9 @@
 ;; ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 ;; Solution
 
-(def euler-20 
-     (apply + (natural->list (factorial 100))))
+(define euler-20 
+    (~> (factorial 100) 
+        natural->list 
+        numbers->sum))
 
 euler-20  ;; → 648
