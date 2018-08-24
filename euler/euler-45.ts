21b21367f533fd05afb3a5c236532c63669008cf
meows <meow@iceland>
2018-08-23 18:38:53 -0700

    update

--- euler/euler-45.ts
+++ euler/euler-45.ts
@@ -7,22 +7,20 @@
 // Find the next triangle number after 40755 that's both pentagonal and 
 // hexagonal.
 
-const pentagon   = (n: number) => 1.5*n*n + 1.5*n
-const hexagon    = (n: number) => 2*n*n + -n
-const triangle   = (n: number) => 0.5*n*n + 0.5*n
-const isPentagon = (x: number) => Number.isSafeInteger((Math.sqrt(24*x + 1) + 1) / 6)
+const pentagon   = (n) => 1.5*n*n + 1.5*n
+const hexagon    = (n) => 2*n*n + -n
+const triangle   = (n) => 0.5*n*n + 0.5*n
+const isPentagon = (x) => Number.isSafeInteger((Math.sqrt(24*x + 1) + 1) / 6)
 
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 // Solve
 
 function euler_45(start = 144) {
-   let i = start
-   let now
+   let now = NaN;
 
-   while (true) {
+   for (let i = start; true; i++) {
       now = hexagon(i)
       if (isPentagon(now)) return now
-      else i++
    }
 }
 
