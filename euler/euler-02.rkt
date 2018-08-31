291f09c9419056e10599d9be0e31c8d6fa3972a3
meows <meow@iceland>
2018-08-31 13:35:32 -0700

    update

--- euler/euler-02.rkt
+++ euler/euler-02.rkt
@@ -1,7 +1,6 @@
 #lang racket
 
 (require (rename-in racket/base [define fn]))
-(require (rename-in racket/base [define def]))
 (require math/number-theory)
 (require threading)
 (require (only-in srfi/26 cut))
@@ -13,22 +12,16 @@
 
 ;; Find the sum of all even Fibonacci numbers under 4 million.
 
-(def max 4000000)
+(define MAX 4000000)
 
 ; s   - trailing fib state tuple
 ; s++ - leading fib state tuple
 ; sum - running sum of even fibonacci numbers
-(fn (euler-2 [s 0] [s++ 1] #:sum [sum 0])
+(fn (euler-0 [s 2] [s++ 8] #:sum [sum 2])
     (if (< max s++)
         sum
-        (euler-2 s++ (+ s s++) 
-                 #:sum (if (odd? s++) sum (+ sum s++)))))
+        (euler-0 s++ 
+                 (+ s (* 4 s++))
+                 #:sum (+ sum s++))))
 
-(define (alt)
-    (for/fold ([s 0] [s++ 1] [sum 0] #:result sum)
-              ([n (in-naturals)] #:break (< max s++))
-              (values s++
-                      (+ s s++)
-                      (if (odd? s++) sum (+ sum s++)))))
-
-(time (euler-2))
+(time (euler-0))
