bed5d14af243e0dab70599ad77f22806dfef3bfd
meows <meow@iceland>
2018-08-31 13:42:59 -0700

    update

--- euler/euler-02.rkt
+++ euler/euler-02.rkt
@@ -11,16 +11,15 @@
 ;; Problem 2
 
 ; Find the sum of all even Fibonacci numbers under 4 million.
-
-(define MAX 4000000)
-
-; We can think of a non-naive solution by observing that the even Fibonacci 
-; terms have an interesting relationship among each other. If fibEven is a
-; function which defines the complete sequence of even Fibonacci numbers, then:
+;
+; If <fibEven> is a function which defines the complete sequence of even 
+; Fibonacci numbers from the naturals <n>:
 ;
 ; fibEven(0) = 2
 ; fibEven(1) = 8
 ; fibEven(n) = 4 * fib(n-1) + fib(n-2)
+
+(define max 4000000)
 
 ;; ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 ;; Solve
@@ -29,7 +28,7 @@
 ; s++ - leading fib state tuple
 ; sum - running sum of even fibonacci numbers
 (fn (euler [s 2] [s++ 8] #:sum [sum 2])
-    (if (< MAX s++)
+    (if (< max s++)
         sum
         (euler s++ 
                (+ s (* 4 s++))
